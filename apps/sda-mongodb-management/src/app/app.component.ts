c19055aafe3f9b263f2fdeb64f5868232bbbae74
Marvin Luchs <marvin@luchs.org>
2019-04-15 21:34:57 +0200

    4 spaces, single quotes, fix and organize on save

--- apps/sda-mongodb-management/src/app/app.component.ts
+++ apps/sda-mongodb-management/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "sda-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+    selector: 'sda-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = "sda-mongodb-management";
+    title = 'sda-mongodb-management';
 }
